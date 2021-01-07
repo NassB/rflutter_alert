8bf4e32fe70f82f8ca148a44e80fcccfa2407afe
Nassim B <bouzouita.nassim@gmail.com>
2021-01-07 16:49:52 +0100

    Add hoverColor for DialogButton

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -7,21 +7,20 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'package:flutter/material.dart';
+
 /// Alert types
 enum AlertType { error, success, info, warning, none }
 
 /// Alert animation types
-enum AnimationType {
-    fromRight,
-    fromLeft,
-    fromTop,
-    fromBottom,
-    grow,
-    shrink
-}
+enum AnimationType { fromRight, fromLeft, fromTop, fromBottom, grow, shrink }
 
 /// Buttons container
-enum ButtonsContainer { row, column }
+enum ButtonsDirection { row, column }
+
+const EdgeInsets defaultAlertPadding = EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0);
 
 /// Library images path
 const String kImagePath = "assets/images";
+
+typedef AlertAnimation = Widget Function(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child);
